7a94e6c4a51ed7a0be5f6af032c951ada8ed1597
l3miage-volozana <74137436+l3miage-volozana@users.noreply.github.com>
2021-03-19 13:30:48 +0100

    AjoutProjet

---
+++ src/app/plateau/plateau.component.ts
@@ -0,0 +1,29 @@
+import { Board_RO, getEmptyBoard, Turn, TileCoords } from './../ReversiDefinitions';
+import { Component, OnInit, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-plateau',
+  templateUrl: './plateau.component.html',
+  styleUrls: ['./plateau.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PlateauComponent implements OnInit {
+  @Input() board : Board_RO = getEmptyBoard();
+  @Input() turn : Turn = "Player1";
+  @Input() playable : boolean[][] = [];
+  @Output() play = new EventEmitter<TileCoords>();
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  tracKByIndex(index:number,e:any):number{
+    return index;
+  }
+
+  PLAY(x:number,y:number):void{
+    this.play.emit([x,y]);
+  }
+}
